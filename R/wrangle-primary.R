7d39991be2eccea07a17f39e3f06885c6a06864a
Rob Lytle <47059344+RobLytle@users.noreply.github.com>
2020-11-09 12:08:17 -0500

    social_class added

--- R/wrangle-primary.R
+++ R/wrangle-primary.R
@@ -3,196 +3,47 @@ library(haven)
 library(stringr)
 library(rio)
 
-#2008
-
-df2008 <- import("data/raw/anes_timeseries_2008.zip", which = "anes_timeseries_2008_rawdata.txt")%>%
-  rename(weight = V081001,
-         primary_vote_choice = V083077a,
-         primary_vote = V083077,
-         therm_dem = V083044a, #1-100, 100 warmest. -99: NA
-         therm_rep = V083044b, #1-100, 100 warmest. -99: NA
-         pid_3 = V083097, #Party ID: Does R think of self as Dem, Rep, Ind or what 0: no pref, 1. Democrat, 2. Republican, 3. Independent
-  )%>%
-  select(weight,
-         primary_vote_choice, # Pulling out the variables we just renamed from the massive dataset
-         primary_vote,
-         therm_dem,
-         therm_rep,
-         pid_3
-  )%>%
-  mutate(pid_3 = recode(pid_3, 
-                        "5" = "No Preference", 
-                        "1" = "Democrat", 
-                        "2" = "Republican", 
-                        "3" = "Independent",
-                        "4" = "Other",
-                        "-8" = NA_character_,
-                        "-9" = NA_character_))%>%
-  mutate(therm_dem = na_if(therm_dem, -9),
-         therm_dem = na_if(therm_dem, -8),
-         therm_dem = na_if(therm_dem, -6))%>%
-  mutate(therm_rep = na_if(therm_rep, -9),
-         therm_rep = na_if(therm_rep, -8),
-         therm_rep = na_if(therm_rep, -6))%>%
-  mutate(therm_inparty = case_when(pid_3 == "Democrat" ~ therm_dem,
-                                   pid_3 == "Republican" ~ therm_rep,
-                                   TRUE ~ NA_integer_))%>%
-  mutate(therm_outparty = case_when(pid_3 == "Democrat" ~ therm_rep,
-                                    pid_3 == "Republican" ~ therm_dem,
-                                    TRUE ~ NA_integer_))%>%
-  mutate(primary_vote_dum = recode(primary_vote,
-                                   "1" = "1",
-                                   "5" = "0",
-                                   "-8" = NA_character_,
-                                   "-9" = NA_character_))%>%
-  mutate(primary_vote_choice = recode(primary_vote_choice, # Unlike the factor vectors above, I'm not reorder this one, as there's nothing substantive about it
-                                      "-1" = NA_character_,
-                                      "1" = "Joe Biden", 
-                                      "2" = "Hillary Clinton",
-                                      "3" = "Chris Dodd",
-                                      "4" = "John Edwards",
-                                      "5" = "Rudy Giuliani",
-                                      "6" = "Mike Gravel",
-                                      "7" = "Mike Huckabee",
-                                      "8" = "Duncan Hunter",
-                                      "9" = "Alan Keyes",
-                                      "10" = "Dennis Kucinich",
-                                      "11" = "John McCain",
-                                      "12" = "Barack Obama",
-                                      "13" = "Ron Paul",
-                                      "14" = "Bill Richardson",
-                                      "15" = "Mitt Romney",
-                                      "16" = "Tom Tancredo",
-                                      "17" = "Fred Thompson",
-                                      "30" = "Someone Else",
-                                      "-8" = NA_character_,
-                                      "-9" = NA_character_
-  ))%>%
-  mutate(cand_party = recode(primary_vote_choice,
-                             "Joe Biden" = "Democrat",
-                             "Hillary Clinton" = "Democrat",
-                             "Chris Dodd" = "Democrat",
-                             "John Edwards" = "Democrat",
-                             "Rudy Giuliani" = "Republican",
-                             "Mike Gravel" = "Democrat",
-                             "Mike Huckabee" = "Republican",
-                             "Alan Keyes" = "Republican",
-                             "Dennis Kucinich" = "Democrat",
-                             "John McCain" = "Republican",
-                             "Barack Obama" = "Democrat",
-                             "Ron Paul" = "Republican",
-                             "Bill Richardson" = "Democrat",
-                             "Tom Tancredo" = "Republican",
-                             "Fred Thompson" = "Republican",
-                             "Someone Else" = "Other/Third Party"))%>%
-  mutate(primary_vote_choice = as.factor(if_else(is.na(primary_vote_choice) & primary_vote_dum == 0, "Didn't Vote", primary_vote_choice)))%>%
-  mutate(primary_vote_simple = case_when(pid_3 != cand_party ~ "Voted in Other Party Primary",
-                                         pid_3 == "Democrat" & primary_vote_choice == "Barack Obama" ~ "Winner",
-                                         pid_3 == "Republican" & primary_vote_choice == "John McCain" ~ "Winner",
-                                         pid_3 != "Indpendent" & primary_vote_choice != "Didn't Vote" ~ "Loser",
-                                         pid_3 != "Indpendent" & primary_vote_choice == "Didn't Vote" ~ "Didn't Vote",
-                                         TRUE ~ NA_character_))%>%
-  select(-primary_vote)%>%
-  mutate(year = "2008")%>%
-  glimpse()
-
-
-#
-##################
-#################
-
-
-
-df2012 <- import("data/raw/anes_timeseries_2012.zip", which = "anes_timeseries_2012_rawdata.txt")%>%
-  rename(weight = weight_full,
-         primary_vote = prevote_primv,
-         primary_vote_choice = prevote_primvwho,
-         therm_dem = ft_dem, #1-100, 100 warmest. -99: NA
-         therm_rep = ft_rep, #1-100, 100 warmest. -99: NA
-         pid_3 = pid_self #Party ID: Does R think of self as Dem, Rep, Ind or what 0: no pref, 1. Democrat, 2. Republican, 3. Independent
-  )%>%
-  select(weight,
-         primary_vote,
-         primary_vote_choice, # Pulling out the variables we just renamed from the massive dataset
-         therm_dem,
-         therm_rep,                                                                                                       
-         pid_3,
-  )%>%
-  mutate(pid_3 = recode(pid_3, 
-                        "0" = "No Preference", 
-                        "1" = "Democrat", 
-                        "2" = "Republican", 
-                        "3" = "Independent",
-                        "5" = "Other",
-                        "-8" = NA_character_,
-                        "-9" = NA_character_))%>%
-  mutate(therm_dem = na_if(therm_dem, -99),
-         therm_dem = na_if(therm_dem, -88),
-         therm_dem = na_if(therm_dem, -89))%>%
-  mutate(therm_rep = na_if(therm_rep, -99),
-         therm_rep = na_if(therm_rep, -88),
-         therm_rep = na_if(therm_rep, -89))%>%
-  mutate(therm_inparty = case_when(pid_3 == "Democrat" ~ therm_dem,
-                                   pid_3 == "Republican" ~ therm_rep,
-                                   TRUE ~ NA_integer_))%>%
-  mutate(therm_outparty = case_when(pid_3 == "Democrat" ~ therm_rep,
-                                    pid_3 == "Republican" ~ therm_dem,
-                                    TRUE ~ NA_integer_))%>%
-  mutate(primary_vote_dum = recode(primary_vote,
-                                   "1" = "1",
-                                   "2" = "0",
-                                   "-8" = NA_character_,
-                                   "-9" = NA_character_))%>%
-  mutate(primary_vote_choice = recode(primary_vote_choice, # Unlike the factor vectors above, I'm not reorder this one, as there's nothing substantive about it
-                                      "-1" = NA_character_,
-                                      "1" = "Mitt Romney", 
-                                      "2" = "Barack Obama",
-                                      "3" = "Rick Santorum",
-                                      "4" = "Newt Gingrich",
-                                      "5" = "Ron Paul",
-                                      "6" = "Rick Perry",
-                                      "7" = "Michelle Bachmann",
-                                      "8" = "Jon Huntsman",
-                                      "9" = "Herman Cain",
-                                      "95" = "Someone Else",
-                                      "-8" = NA_character_,
-                                      "-9" = NA_character_
-  ))%>%
-  mutate(cand_party = recode(primary_vote_choice, .default = "Republican", #supplying Republican as default to save time
-                             "Barack Obama" = "Democrat",
-                             "Someone Else" = "Other/Third Party"))%>%
-  mutate(primary_vote_choice = as.factor(if_else(is.na(primary_vote_choice) & primary_vote_dum == 0, "Didn't Vote", primary_vote_choice)))%>%
-  mutate(primary_vote_simple = case_when(pid_3 != cand_party ~ "Voted in Other Party Primary",
-                                         pid_3 == "Democrat" & primary_vote_choice == "Barack Obama" ~ "Winner",
-                                         pid_3 == "Republican" & primary_vote_choice == "Mitt Romney" ~ "Winner",
-                                         pid_3 != "Indpendent" & primary_vote_choice != "Didn't Vote" ~ "Loser",
-                                         pid_3 != "Indpendent" & primary_vote_choice == "Didn't Vote" ~ "Didn't Vote",
-                                         TRUE ~ NA_character_))%>%
-  mutate(year = 2012)%>%
-  select(colnames(df2008))%>%
-  glimpse()%>%#
-  write_rds("data/tidy-2012.rds")%>%
-  write_csv("data/tidy-2012.csv")
-
 
 #2016
-df2016 <- rio::import("data/raw/anes_timeseries_2016.zip", which = "anes_timeseries_2016_rawdata.txt")%>%
-  rename(weight = V160101,
-         primary_vote = V161021,
-         primary_vote_choice = V161021a,
+df2016 <- rio::import("data/anes_timeseries_2016.zip", which = "anes_timeseries_2016_rawdata.txt")%>%
+  select(weight = V160101,
+         race = V161310x,
+         social_classa = V161307, #Self-reported social class, 1-4 lower, working, middle, upper. -1, -8, -9 NA
+         social_classb = V162133, #Self-reported social class, 1-4 lower, working, middle, upper. -1, -8, -9 NA
+         general_vote = V161026, #Did R vote for president in the general?
+         general_vote_choice = V161027, #For whom did R vote for president (NA if they didn't vote)
+         primary_vote = V161021, #Did R vote in the Primary?
+         primary_vote_choice = V161021a, # for whom did R vote in the primary
          therm_dem = V161095, #1-100, 100 warmest. -99: NA
          therm_rep = V161096, #1-100, 100 warmest. -99: NA
-         pid_7 = V161158x, # Does R think of themeselves as a Dem, Rep, Ind or what? We are using this in addition to registration because we are interested in people's conceptions of party This is coded 1: strong-dem, 2: weak-dem 3: ind-dem, 4: ind, 5: ind-rep, 6: weak-rep, 7: strong-rep -8 DK, -9 NA
+         pid_7 = V161158x, # Does R think of themselves as a Dem, Rep, Ind or what?  This is coded 1: strong-dem, 2: weak-dem 3: ind-dem, 4: ind, 5: ind-rep, 6: weak-rep, 7: strong-rep -8 DK, -9 NA
          pid_3 = V161155#, #Party ID: Does R think of self as Dem, Rep, Ind or what 0: no pref, 1. Democrat, 2. Republican, 3. Independent
   )%>%
-  select(weight,
-         primary_vote,
-         primary_vote_choice, # Pulling out the variables we just renamed from the massive dataset
-         therm_dem,
-         therm_rep,
-         pid_7,
-         pid_3
-  )%>%
+  mutate(social_classa = recode(social_classa, .default = NA_character_,
+                               "1" = "Lower Class",
+                               "2" = "Working Class",
+                               "3" = "Middle Class",
+                               "4" = "Upper Class"))%>%
+  mutate(social_classb = recode(social_classb, .default = NA_character_,
+                                "1" = "Lower Class",
+                                "2" = "Working Class",
+                                "3" = "Middle Class",
+                                "4" = "Upper Class"))%>%
+  mutate(social_class = factor(if_else(is.na(social_classa), social_classb, social_classa)),
+         levels = c("Lower Class",
+                    "Working Class",
+                    "Middle Class",
+                    "Upper Class"))%>% #2016 ANES used two different question setsfor social class, I'm just uniting them here.
+  select(-social_classb,
+         -social_classa)%>% #dropping component parts
+  mutate(race = recode(race, .default = NA_character_,
+                       "1" = "White",
+                       "2" = "Black",
+                       "3" = "Asian/Pacific Islander",
+                       "4" = "Indigenous Americans",
+                       "5" = "Hispanic",
+                       "6" = "Other"))%>%
+  glimpse()
   mutate(pid_7 = na_if(pid_7, -9),
          pid_7 = na_if(pid_7, -8))%>%
   mutate(pid_7_num = as.numeric(pid_7), # Recoding party_id as a factor, making sure to order it in a substantive way.
@@ -203,8 +54,7 @@ df2016 <- rio::import("data/raw/anes_timeseries_2016.zip", which = "anes_timeser
                         "4" = "Independent - Independent", 
                         "5" = "Independent - Republican", 
                         "6" = "Weak Republican", 
-                        "7" = "Strong Republican"
-         ), # We are leaving off `"-9" = NA`, because we have already set na_if(pid_7, -9) above.
+                        "7" = "Strong Republican"), 
          pid_7 = reorder(pid_7, pid_7_num))%>%
   mutate(pid_3 = recode(pid_3, 
                         "0" = "No Preference", 
@@ -244,8 +94,9 @@ df2016 <- rio::import("data/raw/anes_timeseries_2016.zip", which = "anes_timeser
                                       "9" = "A Third Party Candidate",
                                       "-8" = NA_character_, #"Don't Know",
                                       "-9" = NA_character_#"Refused"
-  ))%>%
-  mutate(primary_vote_choice = as.factor(if_else(is.na(primary_vote_choice) & primary_vote_dum == 0, "Didn't Vote", primary_vote_choice)))%>%
+                                      ))%>%
+  mutate(primary_vote_choice = as.factor(if_else(is.na(primary_vote_choice) & primary_vote_dum == 0, "Didn't Vote",
+                                                 primary_vote_choice)))%>%
   mutate(cand_party = recode(primary_vote_choice, .default = NA_character_,
                              "Hillary Clinton" = "Democrat",
                              "Bernie Sanders" = "Democrat",
@@ -264,7 +115,6 @@ df2016 <- rio::import("data/raw/anes_timeseries_2016.zip", which = "anes_timeser
                                          pid_3 != "Indpendent" & primary_vote_choice == "Didn't Vote" ~ "Didn't Vote",
                                          TRUE ~ NA_character_))%>%
   mutate(year = 2016)%>%
-  select(colnames(df2008))%>% #selecting only columns that exist in the 2008 dataset
   glimpse()%>%#
   write_rds("data/tidy-2016.rds")%>%
   write_csv("data/tidy-2016.csv")
@@ -278,23 +128,4 @@ df2016 <- rio::import("data/raw/anes_timeseries_2016.zip", which = "anes_timeser
 ####
 ####
 
-primaries <- rbind(df2016, df2008, df2012)%>%
-  mutate(pid_3 = factor(pid_3, 
-                        levels = c("1" = "Democrat", 
-                                   "2" = "Independent", 
-                                   "3" = "Republican")),
-         primary_vote_choice = as.factor(primary_vote_choice),
-         year = as.factor(year),
-         primary_vote_simple = factor(primary_vote_simple,
-                                      levels = c("1" = "Winner",
-                                                 "2" = "Loser",
-                                                 "3" = "Didn't Vote",
-                                                 "4" = "Voted in Other Party Primary")),
-         cand_party = factor(cand_party,
-                             levels = c("1" = "Democrat",
-                                        "2" = "Republican",
-                                        "3" = "Other/Third Party")),
-         primary_vote_dum = as.numeric(primary_vote_dum))%>%
-  glimpse()%>%
-  write_rds("data/tidy-primaries.rds")%>%
-  write_csv("data/tidy-primaries.csv")
+
